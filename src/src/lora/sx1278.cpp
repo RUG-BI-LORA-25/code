aaf7b1a88f1d2a4ce69d1e73c0da3cdcd5e8cc49
Mihai David <marian1974@gmail.com>
2026-01-11 01:43:27 +0100

    Basic lora module setup

---
+++ src/src/lora/sx1278.cpp
@@ -0,0 +1,24 @@
+#include "lora/sx1278.h"
+
+LORA::LORA(int nss, int reset, int dio0, int dio1)
+    : pin_nss(nss), pin_reset(reset), pin_dio0(dio0), pin_dio1(dio1) 
+{
+    SPIClass spi(LORA_MOSI, LORA_MISO, LORA_SCK);
+    spi.begin();
+
+    (*radio) = new Module(pin_nss, pin_dio0, pin_reset, pin_dio1, spi);
+}
+
+void LORA::begin() {
+    log("[SX1278] Initializing ... ", Serial);
+
+    if(radio == nullptr) {
+        exception("LORA::begin(): radio is nullptr", Serial);
+        return;
+    }
+
+    if(int state = radio->begin(); state != RADIOLIB_ERR_NONE) {
+        exception("Failed to initialize SX1278, error code: " + String(state), Serial);
+        return;
+    }
+}
