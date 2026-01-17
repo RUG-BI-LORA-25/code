c348608af473cf4c024c04b16882c24f6f440361
Boyan K. <boyan@confest.im>
2026-01-17 19:20:37 +0100

    Recovered missing node files

---
+++ node/include/sensors/sensor.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifdef SIMULATION_MODE
+#include "mocks/Arduino.h"
+#else
+#include <Arduino.h>
+#endif
+
+#include "shared.h"
+
+class Sensor {
+    char id[3] = {'U', 'N', '\0'};
+public:
+    explicit Sensor(const char (&newId)[3])
+            : id{newId[0], newId[1], newId[2]} {}
+
+        // optional default ctor with a placeholder ID
+    Sensor() : id{'U','N','K'} {}   // “UNK”
+
+    const char* getID() { return id; }
+    virtual bool begin() = 0;
+    virtual void print(HardwareSerial& serial) = 0;
+    virtual uint8_t serialize(uint8_t* buffer) = 0;
+    virtual void show() = 0;
+};
